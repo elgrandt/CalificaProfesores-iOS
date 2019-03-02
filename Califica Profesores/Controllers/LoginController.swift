a380e5e59a7a47f4a40015c1ed20805da92a6b6d
Dylan Tasat <grandt@Dylans-Mac.local>
2019-03-02 01:10:27 -0300

    Initial Commit

---
+++ Califica Profesores/Controllers/LoginController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginController.swift
+//  Califica Profesores
+//
+//  Created by Dylan Tasat on 02/03/2019.
+//  Copyright © 2019 Dylan Tasat. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
