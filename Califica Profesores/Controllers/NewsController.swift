adc0c01e00bba3e90b67806e122e4452744f8187
Dylan Tasat <grandt@Dylans-Mac.local>
2019-04-22 17:38:04 -0300

    Removed transparency to icons

--- Califica Profesores/Controllers/NewsController.swift
+++ Califica Profesores/Controllers/NewsController.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 import Firebase
-import FirebaseUI
 import CardParts
 
 class NewsController: UIViewController {
--- Califica Profesores/Controllers/SideMenuController.swift
+++ Califica Profesores/Controllers/SideMenuController.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 import Firebase
-import FirebaseUI
 import FBSDKLoginKit
 import GoogleSignIn
 
