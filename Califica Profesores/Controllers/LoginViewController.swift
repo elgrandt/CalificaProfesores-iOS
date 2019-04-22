4bde508714f8d38e53e45ff94d92c5c09c877e50
Dylan Tasat <grandt@Dylans-Mac.local>
2019-04-22 00:49:18 -0300

    Finished login refactor

--- Califica Profesores/Controllers/LoginViewController.swift
+++ Califica Profesores/Controllers/LoginViewController.swift
@@ -19,6 +19,7 @@ func showError(error : String, title : String = "Error", self : UIViewController
     let alertController = UIAlertController(title: title, message: error, preferredStyle: .alert)
     let okayAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
     alertController.addAction(okayAction)
+
     self.present(alertController, animated: true, completion: nil)
 }
 
@@ -26,6 +27,9 @@ class LoginViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDeleg
     
     let nextController = "SideMenuController"
     @IBOutlet weak var loading: LoadingView!
+    @IBOutlet weak var facebookButton: UIView!
+    @IBOutlet weak var googleButton: UIView!
+    @IBOutlet weak var emailButton: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,6 +37,15 @@ class LoginViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDeleg
         
         GIDSignIn.sharedInstance().uiDelegate = self
         GIDSignIn.sharedInstance().delegate = self
+        
+        let fbtap = UITapGestureRecognizer(target: self, action: #selector(facebookLogin))
+        facebookButton.addGestureRecognizer(fbtap)
+        
+        let ggtap = UITapGestureRecognizer(target: self, action: #selector(googleLogin))
+        googleButton.addGestureRecognizer(ggtap)
+        
+        let emtap = UITapGestureRecognizer(target: self, action: #selector(emailLogin))
+        emailButton.addGestureRecognizer(emtap)
         
         loading.isHidden = false
     }
@@ -56,25 +69,28 @@ class LoginViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDeleg
         self.present(controller!, animated: animated, completion: nil)
     }
     
-    @IBAction func facebookLogin(sender: UIButton) {
+    @objc func facebookLogin() {
         let fbLoginManager = FBSDKLoginManager()
         fbLoginManager.logIn(withReadPermissions: ["public_profile", "email"], from: self) { (result, error) in
-            if let error = error {
-                showError(error: error.localizedDescription, title: "Login error", self: self)
+            if (error != nil) {
                 return
             }
             self.firebaseFacebookLogin()
         }
     }
     
-    @IBAction func googleLogin(_ sender: UIButton) {
+    @objc func googleLogin() {
         GIDSignIn.sharedInstance()?.signIn()
+    }
+    
+    @objc func emailLogin() {
+        let controller = self.storyboard?.instantiateViewController(withIdentifier: "SignInWithEmail")
+        self.navigationController?.pushViewController(controller!, animated: true)
     }
     
     // Facebook
     func firebaseFacebookLogin() {
         guard let accessToken = FBSDKAccessToken.current() else {
-            showError(error: "Failed to get access token", title: "Login error", self: self)
             return
         }
         
@@ -84,8 +100,7 @@ class LoginViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDeleg
     
     // Google
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) {
-        if let error = error {
-            showError(error: error.localizedDescription, title: "Login error", self: self)
+        if (error != nil) {
             return
         }
         
@@ -221,6 +236,71 @@ class RegisterWithEmailController : UIViewController {
             }
         } else {
             showError(error: "Password do not match", title: "Register error", self: self)
+        }
+    }
+}
+
+class SendRecoveryEmailController : UIViewController {
+    
+    @IBOutlet weak var emailInput: UITextField!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var loading: LoadingView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        // AGREGO PARA QUE SE CIERRE EL TECLADO TOCANDO AFUERA
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard(sender:UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    
+    @objc func keyboardWillShow(notification: Notification) {
+        let keyboardRect = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+        let new_inset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardRect!.height, right: 0)
+        scrollView.contentInset = new_inset
+        scrollView.scrollIndicatorInsets = new_inset
+    }
+    
+    @objc func keyboardWillHide(notification: Notification) {
+        let new_inset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        scrollView.contentInset = new_inset
+        scrollView.scrollIndicatorInsets = new_inset
+    }
+    
+    @IBAction func sendRecovery(_ sender: Any) {
+        view.endEditing(true)
+        loading.isHidden = false
+        Auth.auth().sendPasswordReset(withEmail: emailInput.text!) { (error) in
+            self.loading.isHidden = true
+            if let error = error {
+                showError(error: error.localizedDescription, title: "Recovery error", self: self)
+                return
+            }
+            let alertController = UIAlertController(title: "Success", message: "We sent a mail to with a link to change your password.", preferredStyle: .alert)
+            let okayAction = UIAlertAction(title: "OK", style: .cancel, handler: { action in
+                let controller = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "Login")
+                UIApplication.shared.keyWindow?.rootViewController = controller
+            })
+            alertController.addAction(okayAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
+}
+
+extension UIView {
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
         }
     }
 }
